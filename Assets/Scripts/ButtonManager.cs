a035e33c6ba1a80834c7b737cca0f49d2f25b100
tousleboy <63148826+tousleboy@users.noreply.github.com>
2021-11-06 21:50:58 +0900

    ds4 can select and push buttons

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonManager : MonoBehaviour/*, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler*/
+{
+    public GameObject[] buttons;
+    public string decisionKey = "Jump";
+    int pointer = 0;
+    bool wait = false;
+    float waitTime = 0.4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Color c = buttons[pointer].GetComponent<Button>().colors.pressedColor;
+        buttons[pointer].GetComponent<Image>().color = c;
+        //Button b = buttons[pointer].GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!AllActive())
+        {
+            return;
+        }
+        if(!wait)
+        {
+            if(Input.GetAxis("Vertical") != 0)
+            {
+                float axisV = Input.GetAxisRaw("Vertical");
+
+                //Button b = buttons[pointer].GetComponent<Button>();
+                //b.OnPointerExit(EventSystems.PointerEventData);
+                Color c = buttons[pointer].GetComponent<Button>().colors.normalColor;
+                buttons[pointer].GetComponent<Image>().color = c;
+
+                if(axisV > 0) pointer = (pointer - 1 + buttons.Length) % buttons.Length;
+                else pointer = (pointer + 1) % buttons.Length;
+
+                Debug.Log(pointer);
+
+                c = buttons[pointer].GetComponent<Button>().colors.pressedColor;
+                buttons[pointer].GetComponent<Image>().color = c;
+                //b = buttons[pointer].GetComponent<Button>();
+                //b.OnPointerEnter(EventSystems.PointerEventData);
+                wait = true;
+                Invoke("StopWait", waitTime);
+            }
+        }
+
+        if(Input.GetButtonDown(decisionKey))
+        {
+            Debug.Log("x");
+            Button b = buttons[pointer].GetComponent<Button>();
+            Color c = b.colors.pressedColor;
+            buttons[pointer].GetComponent<Image>().color = c;
+            b.onClick.Invoke();
+            //b.OnPointerDown(EventSystems.PointerEventData);
+            wait = true;
+        }
+    }
+
+    void StopWait()
+    {
+        wait = false;
+    }
+
+    bool AllActive()
+    {
+        int i;
+        bool ret = true;
+        for(i = 0; i < buttons.Length; i++)
+        {
+            ret = ret && buttons[i].activeSelf;
+        }
+        return ret;
+    }
+
+    /*public void OnPointerEnter(PointerEventData exentData)
+    {
+
+    }
+
+    public void OnPointerExit(PointerEventData exentData)
+    {
+
+    }
+
+    public void OnPointerDown(PointerEventData exentData)
+    {
+
+    }*/
+}
